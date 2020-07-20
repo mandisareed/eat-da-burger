a17abd98cfb5b75ea0f54e9eeb9a0d80177cf9fe
Mandisa Reed <mandisa.reed@gmail.com>
2020-07-19 22:13:00 -0400

    cleaned up code in db folder, setup connection to db and orm file

---
+++ db/seeds.sql
@@ -0,0 +1,4 @@
+INSERT INTO burgers (burger_name, devoured) VALUES ('Black Bean Burger', FALSE);
+INSERT INTO burgers (burger_name, devoured) VALUES ('Hamburger', FALSE);
+INSERT INTO burgers (burger_name, devoured) VALUES ('Kobe', FALSE);
+
