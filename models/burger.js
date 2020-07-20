324ef71e6c8621cc15f5ff4537d0b616e0127697
Mandisa Reed <mandisa.reed@gmail.com>
2020-07-19 22:22:24 -0400

    created models folder with js to call the ORM functions

---
+++ models/burger.js
@@ -0,0 +1,24 @@
+// Import the ORM to create functions that will interact with the database.
+const orm = require("../config/orm.js");
+
+const burger = {
+  all: (cb) => {
+    orm.all("burgers", (res) => {
+      cb(res);
+    });
+  },
+  // The variables cols and vals are arrays.
+  create: (newBurger, cb) => {
+    orm.create("burgers", newBurger, (res) => {
+      cb(res);
+    });
+  },
+  update: (burgerData, criteria, cb) => {
+    orm.update("burgers", burgerData, criteria, (res) => {
+      cb(res);
+    });
+  },
+};
+
+// Export the database functions for the controller (catsController.js).
+module.exports = burger;
